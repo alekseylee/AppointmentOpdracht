6a0c6abc39fdc007d9ace66476cb52122c4c1538
alekseylee <aleksey.lee@gmail.com>
2023-03-01 10:27:05 +0100

    1.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,14 +3,18 @@ package org.example;
 import org.example.model.Appointment;
 import org.example.service.AppointmentService;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Optional;
 
 public class Main {
-    public static void main(String[] args) {
-        Appointment client1 = new Appointment( "GP", "Head pain", 28, 02, 2023, 13, 25);
-        Appointment client2 = new Appointment( "Hand Surgery", "Break hand", 28, 02, 2023, 10, 01);
-        Appointment client3 = new Appointment( "Dentist", "Veneers Procedure", 01, 03, 2023, 9, 44);
-        Appointment client4 = new Appointment( "Ophthalmologist", "Vision test", 05, 01, 2023, 8, 25);
+    public static void main(String[] args) throws ParseException {
+
+        Appointment client1 = new Appointment("GP", "Head pain", 28, 02, 2023, 13, 25);
+        Appointment client2 = new Appointment("Hand Surgery", "Break hand", 28, 02, 2023, 10, 01);
+        Appointment client3 = new Appointment("Dentist", "Veneers Procedure", 01, 03, 2023, 9, 44);
+        Appointment client4 = new Appointment("Ophthalmologist", "Vision test", 05, 01, 2023, 8, 25);
 
         AppointmentService as = new AppointmentService();
 
@@ -36,9 +40,21 @@ public class Main {
         Optional<Appointment> appointmentToUpdate = as.readAppointment("28_2_2023__10_1");
         if (appointmentToUpdate.isPresent()) {
             Appointment foundAppointment = appointmentToUpdate.get();
-            foundAppointment.setHour(foundAppointment.getHour()+1);
+            foundAppointment.setHour(foundAppointment.getHour() + 1);
             as.updateAppointment(foundAppointment);
         }
 
+        Date date = new Date();
+        java.sql.Date myDate = convertDate(date);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd_MM_yyyy__HH_mm");
+        Date utildate = sdf.parse("28_2_2023__10_1");
+        System.out.println(utildate);
     }
+
+    private static java.sql.Date convertDate(java.util.Date date) {
+        java.sql.Date myDate = new java.sql.Date(date.getTime());
+        return myDate;
+    }
+
 }
